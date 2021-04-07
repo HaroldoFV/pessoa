d5bb5d7e5bc41669ab35b0a9d15005c6199817de
HaroldoFV <haroldovinente@gmail.com>
2021-04-06 23:39:42 -0300

    adicionando classe Pessoa

---
+++ Pessoa/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Pessoa.Models
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+        }
+
+        public long Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
